2888bb69a0144e5a06134c4ab873cf876090959d
Lucie Meresse <lumu59@gmail.com>
2010-09-23 20:44:17 +0200

    Mise a jour widget quarterslider

--- awesomix/widgets/optionwidget.py
+++ awesomix/widgets/optionwidget.py
@@ -5,14 +5,20 @@ from math import cos, sin, pi
 class MTOptionWidget(MTMovableCircleWidget):
     def on_update(self):
         super(MTOptionWidget, self).on_update()
-        print self.children
         angle = 0
         r = self.radius + 50
+        anglestep = 2 * pi / len(self.children)
         for index,child in enumerate(self.children):
+            '''
             child.center = (
                     (self.x + r * sin(angle)),
                     (self.y + r * cos(angle)))
-            angle += 2* pi / len(self.children)
+            '''
+            child.pos = self.pos
+            child.start_angle = angle
+            child.end_angle = angle + anglestep
+            child.inner_radius = self.radius
+            angle += anglestep
 
 
 
