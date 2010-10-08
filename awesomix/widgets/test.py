137099dddd7efeff4ee470e8e56c54acf44ea222
nico <nico@nico-laptop.(none)>
2010-10-08 18:25:52 +0200

    mise a jour du fichier test

--- awesomix/widgets/test.py
+++ awesomix/widgets/test.py
@@ -2,6 +2,7 @@ from pymt import *
 from optionwidget import MTOptionWidget
 from quarter import MTQuarter
 from quarterbutton import MTQuarterButton
+from quarterslider import MTQuarterSlider
 
 if __name__ == '__main__':
     win = getWindow()
@@ -11,7 +12,7 @@ if __name__ == '__main__':
         circle.add_widget(slider)
     win.add_widget(circle)
     circle2 = MTOptionWidget(pos=(100, 100))
-    circle2.add_widget(MTQuarterSliderButton(color=(1,0,0)))
-    circle2.add_widget(MTQuarterSliderButton(color=(0,1,0)))
+    circle2.add_widget(MTQuarterSlider(color=(0,1,0)))
+    circle2.add_widget(MTQuarterButton(color=(1,0,0)))
     win.add_widget(circle2)
     runTouchApp()
