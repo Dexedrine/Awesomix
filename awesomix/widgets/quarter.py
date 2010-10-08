a755eefba8dd4979a7ccc17a916a8411dbd0d845
nico <nico@nico-laptop.(none)>
2010-10-08 18:26:23 +0200

    mise en place du zoom

--- awesomix/widgets/quarter.py
+++ awesomix/widgets/quarter.py
@@ -4,14 +4,20 @@ from math import cos, sin, radians
 class MTQuarter(MTWidget):
     def __init__(self, **kwargs):
         super(MTQuarter, self).__init__(**kwargs)
+        self.zoom = kwargs.get('zoom', 1.)
+        self.lzoom = self.zoom
         self.inner_radius = kwargs.get('inner_radius', 50)
-        self.outer_radius = kwargs.get('outer_radius', 100)
+        self._outer_radius = kwargs.get('outer_radius', 100)
         self.start_angle = kwargs.get('start_angle', 0)
         self.end_angle = kwargs.get('end_angle', 360 / 10)
 
         self.color = kwargs.get('color', (255, 165, 0))
         self.bg_color = kwargs.get('bg_color', (250, 250, 250))
         self._active = False
+
+    @property
+    def outer_radius(self):
+        return self._outer_radius + ((self.lzoom - 1) * 10)
 
     def collide_point(self, x, y):
         cx, cy = self.pos
@@ -35,6 +41,10 @@ class MTQuarter(MTWidget):
                 outer_radius=self.outer_radius,
                 start_angle=self.start_angle,
                 sweep_angle=self.end_angle - self.start_angle)
+
+    def on_update(self):
+        super(MTQuarter, self).on_update()
+        self.lzoom = interpolate(self.lzoom, self.zoom, 5)
 
 
 
