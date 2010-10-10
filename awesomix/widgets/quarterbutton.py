c7fe2f3b45df09568344620936a8f64216306c9f
Lucie Meresse <lumu59@gmail.com>
2010-10-10 21:44:12 +0200

    diminution du code -> factorisation dans MTQuarter

--- awesomix/widgets/quarterbutton.py
+++ awesomix/widgets/quarterbutton.py
@@ -4,24 +4,6 @@ from quarter import MTQuarter
 class MTQuarterButton(MTQuarter):
     def __init__(self, **kwargs):
         super(MTQuarterButton, self).__init__(**kwargs)
-        self._active_touch = None
-
-    def on_touch_down(self, touch):
-        if self._active:
-            return super(MTQuarterButton, self).on_touch_down(touch)
-        if not self.collide_point(touch.x, touch.y):
-            return
-        self.zoom = 5
-        self._active = True
-        self._active_touch = touch.uid
-        return True
-
-    def on_touch_up(self, touch):
-        if touch.uid != self._active_touch:
-            return
-        self.zoom = 1
-        self._active = False
-        return True
 
 if __name__ == '__main__':
     w = getWindow()
--- awesomix/widgets/quarterslider.py
+++ awesomix/widgets/quarterslider.py
@@ -6,7 +6,6 @@ class MTQuarterSlider(MTQuarter):
         super(MTQuarterSlider, self).__init__(**kwargs)
         self.slider_radius = kwargs.get('slider_radius', self.inner_radius + 10)
         self.slider_color = kwargs.get('slider_color', (255, 0, 255))
-        
 
     def draw(self):
         super(MTQuarterSlider, self).draw()
@@ -17,28 +16,10 @@ class MTQuarterSlider(MTQuarter):
                 start_angle=self.start_angle,
                 sweep_angle=self.end_angle)
 
-    def update_slider(self, x, y):
+    def update_widget(self, x, y):
         d = Vector(self.pos).distance((x, y))
         d = boundary(self.inner_radius, self.outer_radius, d)
         self.slider_radius = d
-
-    def on_touch_down(self, touch):
-        if super(MTQuarterSlider,self).on_touch_down(touch):
-            return True
-        print 'on_touch_down()', touch.pos
-        if not self.collide_point(*touch.pos):
-            return
-        self.update_slider(touch.x, touch.y)
-        return True
-
-    def on_touch_move(self, touch):
-        if super(MTQuarterSlider,self).on_touch_move(touch):
-            return True
-        print 'on_touch_move()', touch.pos
-        if not self.collide_point(*touch.pos):
-            return
-        self.update_slider(touch.x, touch.y)
-        return True
 
 
 if __name__ == '__main__':
