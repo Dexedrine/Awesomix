3813e6de9267295aa6243d4f8376246580a13bf9
nico <nico@nico-Studio-1749.(none)>
2010-10-14 20:49:38 +0200

    Modification du fichier test

--- awesomix/lib/test.py
+++ awesomix/lib/test.py
@@ -2,10 +2,12 @@ from sound import Sound
 from soundmanager import SoundManager
 from pygamesoundmanager import PygameSoundManager
 from time import sleep
+import sys
 
 if __name__ == '__main__':
     pygame = PygameSoundManager()
-    sound = Sound(pygame, '/home/nico/toto.mp3')
+    sound = Sound(pygame, sys.argv[1])
     sound.load()
     sound.play()
     sleep(10)
+    print(sound.get_volume())
