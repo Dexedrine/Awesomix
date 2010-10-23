2fe9e1ce6bbfa6bc91eb7844c896452d44e9d6ff
nico <nico@nico-Studio-1749.(none)>
2010-10-23 23:37:44 +0200

    Ajout des event on press et on release pour circlewidget Changement pour que les variables color et bg_color fonctionnent dans la méthode draw

--- awesomix/widgets/circlewidget.py
+++ awesomix/widgets/circlewidget.py
@@ -4,10 +4,19 @@ class MTCircleWidget(MTWidget):
     def __init__(self, **kwargs):
         super(MTCircleWidget, self).__init__(**kwargs)
         self.radius = kwargs.get('radius', 50)
-        self.color = kwargs.get('color', (0, 1, 0))
-        self.bg_color = kwargs.get('bg_color', (0, 0, 1))
+        self.color = kwargs.get('color', (0.5, 0, 0.8))
+        self.bg_color = kwargs.get('bg_color', (0.8, 0, 0.5))
         self._active = False
         self._active_touch = None
+
+        self.register_event_type('on_press')
+        self.register_event_type('on_release')
+
+    def on_press(self, *largs):
+        pass
+
+    def on_release(self, *largs):
+        pass
 
     def collide_point(self, x, y):
         curpos = Vector(self.pos)
@@ -24,6 +33,7 @@ class MTCircleWidget(MTWidget):
             return
         self._active = True
         self._active_touch = touch.uid
+        self.dispatch_event('on_press', touch)
         return True
 
     def on_touch_up(self, touch):
@@ -32,15 +42,30 @@ class MTCircleWidget(MTWidget):
         if touch.uid != self._active_touch:
             return
         self._active = False
+        self.dispatch_event('on_release', touch)
         return True
 
     def draw(self):
         if self._active:
-            set_color(0, 1, 0)
+            set_color(*self.bg_color)
         else:
-            set_color(0, 0, 1)
+            set_color(*self.color)
         drawCircle(pos=self.pos, radius=self.radius)
 
     @property
     def active(self):
         return self._active
+
+if __name__ == '__main__':
+    win = getWindow()
+    for x in xrange(4):
+        circle = MTCircleWidget(pos=((win.width / 4.)*x + 50., win.height / 2.))
+        win.add_widget(circle)
+    
+    circle2 = MTCircleWidget(pos=(win.width / 2., 100))
+    win.add_widget(circle2)
+    @circle2.event
+    def on_press(*largs):
+        print("on press")
+
+    runTouchApp()
