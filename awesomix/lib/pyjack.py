0c964497a0e0e574c7cd3c918cc4c5c88168bd4f
nico <nico@nico-Studio-1749.(none)>
2010-11-14 19:45:26 +0100

    Gestion des exceptions dans pyjack

--- awesomix/lib/pyjack.py
+++ awesomix/lib/pyjack.py
@@ -1,10 +1,15 @@
-import jack
+import jack, sys
+
 class Pyjack(object):
     def __init__(self):
         super(Pyjack, self).__init__()
 
     def connect(self):
-        jack.attach("sooperlooper")
-        jack.activate()
-        jack.connect("sooperlooper:common_out_1", "alsa_pcm:playback_1")
-        jack.connect("sooperlooper:common_out_2", "alsa_pcm:playback_2")
+        try :
+            jack.attach("sooperlooper")
+            jack.activate()
+            jack.connect("sooperlooper:common_out_1", "alsa_pcm:playback_1")
+            jack.connect("sooperlooper:common_out_2", "alsa_pcm:playback_2")
+        except :
+            print("Run jack and sooperlooper")
+            sys.exit(1) 
