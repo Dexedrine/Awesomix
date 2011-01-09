1ea2c47a7fd0cad70a8eb222d47e1b3f6565ae09
nico <nico@nico-Studio-1749.(none)>
2011-01-09 11:49:36 +0100

    Lancement de sooperlooper et de Awesomix par ce fichier start.py

--- start.py
+++ start.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python
 # fichier principal
+
+import subprocess, sys
+
+#Lancement de qjackctl
+#qjackctl = subprocess.Popen("qjackctl")
+#qjackctl.poll()
+
+#Lancement de sooperlooper
+sooperlooper = subprocess.Popen("sooperlooper")
+sooperlooper.poll()
+
+awesomix = subprocess.Popen([sys.executable, 'awesomix/test.py'] + sys.argv[1:])
+awesomix.wait()
